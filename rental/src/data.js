6f270b561edefdc167273c625d744cb2087603d4
Alexis <alexis.borja@gmail.com>
2023-03-29 21:31:47 -0700

    amenities

--- rental/src/data.js
+++ rental/src/data.js
@@ -1,7 +1,22 @@
 import workspace from './assets/amenities/dedicated_workspace.jpeg';
+import toiletries2 from './assets/amenities/toiletries2.jpeg';
 import toiletries from './assets/amenities/toiletries.jpeg';
+import cleaning from './assets/amenities/cleaning.jpeg';
+import stocked_kitchen1 from './assets/amenities/stocked_kitchen1.jpeg';
+import stocked_kitchen from './assets/amenities/stocked_kitchen.jpeg';
+import washer from './assets/amenities/washer.jpeg';
+import wifi from './assets/amenities/wifi.jpeg';
+import linen from './assets/amenities/linen.jpg';
 
 export const amenitiesData = [
   { photo: toiletries, title: 'toiletries' },
+  { photo: toiletries2, title: 'toiletries2' },
+  { photo: cleaning, title: 'cleaning' },
+  { photo: washer, title: 'washer' },
+  { photo: wifi, title: 'wifi' },
+  { photo: linen, title: 'linen' },
+  { photo: stocked_kitchen, title: 'stocked_kitchen' },
+  { photo: stocked_kitchen1, title: 'stocked_kitchen1' },
   { photo: workspace, title: 'dedicated workspace' },
+
 ];
