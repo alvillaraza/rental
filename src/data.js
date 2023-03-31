0bf9b4ed97a3ec4f1fda8ea3d253aecc52a4e147
Alexis <alexis.borja@gmail.com>
2023-03-30 19:45:40 -0700

    updated amenities photo sizes

--- src/data.js
+++ src/data.js
@@ -1,22 +1,27 @@
 import workspace from './assets/amenities/dedicated_workspace.jpeg';
 import toiletries2 from './assets/amenities/toiletries2.jpeg';
-import toiletries from './assets/amenities/toiletries.jpeg';
+// import toiletries from './assets/amenities/toiletries.jpeg';
 import cleaning from './assets/amenities/cleaning.jpeg';
 import stocked_kitchen1 from './assets/amenities/stocked_kitchen1.jpeg';
-import stocked_kitchen from './assets/amenities/stocked_kitchen.jpeg';
+// import stocked_kitchen from './assets/amenities/stocked_kitchen.jpeg';
 import washer from './assets/amenities/washer.jpeg';
 import wifi from './assets/amenities/wifi.jpeg';
 import linen from './assets/amenities/linen.jpg';
+import smartlock from './assets/amenities/smartlock.webp';
+import firstaid from './assets/amenities/firstaid.jpeg';
+
 
 export const amenitiesData = [
-  { photo: toiletries, title: 'toiletries' },
-  { photo: toiletries2, title: 'toiletries2' },
-  { photo: cleaning, title: 'cleaning' },
-  { photo: washer, title: 'washer' },
-  { photo: wifi, title: 'wifi' },
-  { photo: linen, title: 'linen' },
-  { photo: stocked_kitchen, title: 'stocked_kitchen' },
-  { photo: stocked_kitchen1, title: 'stocked_kitchen1' },
-  { photo: workspace, title: 'dedicated workspace' },
+  { photo: smartlock, title: 'Self Check-in' },
+  { photo: linen, title: 'Fresh Linens' },
+  { photo: wifi, title: 'Complimentary High Speed Wifi' },
+  { photo: stocked_kitchen1, title: 'Fully Stocked Kitchen' },
+  // { photo: toiletries, title: 'toiletries' },
+  { photo: workspace, title: 'Dedicated Workspace' },
+  { photo: toiletries2, title: 'Toiletries' },
+  { photo: washer, title: 'In-unit Washer and Dryer' },
+  { photo: cleaning, title: 'Professional Cleaning' },
+  // { photo: stocked_kitchen, title: 'stocked_kitchen' },
+  { photo: firstaid, title: 'Safety Features' },
 
 ];
